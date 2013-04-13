f5c1ab3f8e2e77ea89ae6f8555b0601f2f955b82
Andreas Politz <politza@hochschule-trier.de>
2013-04-13 13:30:52 +0200

    Revert "Changed outputting metadata dates to ctime."
    This reverts commit 094be6031248ba85b63a325607cda33f70f5d9e1.

--- src/epdfinfo.c
+++ src/epdfinfo.c
@@ -816,7 +816,6 @@ cmd_metadata (const ctxt_t *ctx, const arg_t *args)
   gchar *md[6];
   gchar *title;
   int i;
-  struct tm *tmp;
 
   OK_BEG ();
   
@@ -841,11 +840,9 @@ cmd_metadata (const ctxt_t *ctx, const arg_t *args)
     }
 
   date = poppler_document_get_creation_date (doc);
-  tmp = ctime (&date);
-  printf ("%s:", tmp ? tmp : "");
+  printf ("%lld:", (gint64) date);
   date = poppler_document_get_modification_date (doc);
-  tmp = ctime (&date);
-  printf ("%s\n", tmp ? tmp : "");
+  printf ("%lld\n", (gint64) date);
 #endif
   OK_END ();
 #else
