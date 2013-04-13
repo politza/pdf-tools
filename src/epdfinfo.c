6b2ab99c6903568b78347ed3f2b32b8b2c50acb8
Andreas Politz <politza@hochschule-trier.de>
2013-04-13 13:53:25 +0200

    Fixed time output.

--- src/epdfinfo.c
+++ src/epdfinfo.c
@@ -841,11 +841,11 @@ cmd_metadata (const ctxt_t *ctx, const arg_t *args)
     }
 
   date = poppler_document_get_creation_date (doc);
-  time_str = ctime (&date);
-  printf ("%s:", time_str ? time_str : "");
+  time_str = strchomp (ctime (&date));
+  print_escaped (time_str ? time_str : "", FALSE);
   date = poppler_document_get_modification_date (doc);
-  time_str = ctime (&date);
-  printf ("%s\n", time_str ? time_str : "");
+  time_str = strchomp (ctime (&date));
+  print_escaped (time_str ? time_str : "", TRUE);
 #endif
   OK_END ();
 #else
