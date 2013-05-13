c5ffd0a4b9bffa946f5e1e711037ab201885bee1
Andreas Politz <politza@hochschule-trier.de>
2013-05-13 05:09:36 +0200

    Move window functions here.

--- lisp/pdf-util.el
+++ lisp/pdf-util.el
@@ -765,6 +765,47 @@ colors, otherwise light."
                 (when (and f (frame-live-p f))
                   (delete-frame f)))))))))
 
+(defun pdf-util-window-attach (awindow &optional window)
+  "Attach AWINDOW to WINDOW.
+
+This has the following effect.  Whenever WINDOW, defaulting to
+the selected window, stops displaying the buffer it currently
+displays (e.g., by switching buffers or because it was deleted)
+AWINDOW is deleted."
+  (unless window (setq window (selected-window)))
+  (let ((abuffer (window-buffer awindow))
+        (buffer (window-buffer window))
+        (hook (make-symbol "window-attach-hook")))
+    (fset hook
+          (lambda ()
+            (when (or (not (window-live-p window))
+                      (not (eq buffer (window-buffer window))))
+              (remove-hook 'window-configuration-change-hook
+                           hook)
+              ;; Deleting windows inside wcch leads to errors in
+              ;; windows.el .
+              (run-with-timer
+               0 nil (lambda (win)
+                       (when (and (window-live-p win)
+                                  (not (eq win (selected-window))))
+                         (delete-window win)))
+               awindow))))
+    (add-hook 'window-configuration-change-hook hook)))
+
+(defun display-buffer-split-below-and-attach (buf alist)
+  (let ((window (selected-window))
+        (height (cdr (assq 'window-height alist)))
+        newwin)
+    (when height
+      (when (floatp height)
+        (setq height (round (* height (frame-height)))))
+      (setq height (- (max height window-min-height))))
+    (setq newwin (window--display-buffer
+                  buf
+                  (split-window-below height)
+                  'window alist display-buffer-mark-dedicated))
+    (pdf-util-window-attach newwin window)
+    newwin))
 
 (provide 'pdf-util)
 
