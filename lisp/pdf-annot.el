585202d11f81163fd3aed488a39934d3d359e514
Andreas Politz <politza@hochschule-trier.de>
2015-01-02 21:43:18 +0100

    Handle recent changes in org.
    * lisp/pdf-annot.el (pdf-annot-print-annotation-latex): Call the org
      code in a temp buffer, to avoid errors with the read-only PDF
      buffer.

--- lisp/pdf-annot.el
+++ lisp/pdf-annot.el
@@ -1273,13 +1273,14 @@ by a header."
               (make-directory temporary-file-directory))
             (unless data
               (setq tempfile (make-temp-file "pdf-annot" nil ".png"))
-              (funcall
-               (cl-case org-latex-create-formula-image-program
-                 (dvipng 'org-create-formula-image-with-dvipng)
-                 (imagemagick 'org-create-formula-image-with-imagemagick)
-                 (t (error
-                     "Invalid value of `org-latex-create-formula-image-program'")))
-               contents tempfile org-format-latex-options t)
+              (with-temp-buffer
+                (funcall
+                 (cl-case org-latex-create-formula-image-program
+                   (dvipng 'org-create-formula-image-with-dvipng)
+                   (imagemagick 'org-create-formula-image-with-imagemagick)
+                   (t (error
+                       "Invalid value of `org-latex-create-formula-image-program'")))
+                 contents tempfile org-format-latex-options t))
               (setq data (pdf-util-munch-file tempfile))
               (if (and (> (length data) 3)
                        (equal (substring data 1 4)
