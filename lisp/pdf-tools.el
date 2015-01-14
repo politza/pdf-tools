001888c269e1e55c34fad61ccd149c5ab39930d1
Andreas Politz <politza@hochschule-trier.de>
2015-01-14 23:47:44 +0100

    Changed server build function.

--- lisp/pdf-tools.el
+++ lisp/pdf-tools.el
@@ -108,16 +108,17 @@ PDF buffers."
 ;; * ================================================================== *
 
 
-(defun pdf-tools-rebuild-server (&optional directory)
-  (interactive (list nil))
-  (unless (executable-find "make")
-    (error "Executable `make' command not found"))
-  (let ((compilation-auto-jump-to-first-error nil)
-        (compilation-scroll-output t)
-        (directory (or directory
-                       (file-name-directory pdf-info-epdfinfo-program))))
-    (compile 
-     (format "make -skC '%s' elpa-all" directory))))
+(defun pdf-tools-build-server ()
+  (interactive)
+  (if (file-executable-p pdf-info-epdfinfo-program)
+      (message "%s" "Server already build.")
+    (unless (executable-find "make")
+      (error "Executable `make' command not found"))
+    (let ((compilation-auto-jump-to-first-error nil)
+          (compilation-scroll-output t)
+          (directory (file-name-directory pdf-info-epdfinfo-program)))
+      (compile 
+       (format "make -skC '%s' melpa" directory)))))
 
 (defun pdf-tools-pdf-buffer-p (&optional buffer)
   "Return non-nil if BUFFER contains a PDF document."
@@ -166,7 +167,7 @@ See `pdf-view-mode' and `pdf-tools-enabled-modes'."
   (interactive)
   (unless (file-executable-p pdf-info-epdfinfo-program)
     (when (y-or-n-p "Need to build the server, do it now ? ")
-      (pdf-tools-rebuild-server))
+      (pdf-tools-build-server))
     (error "No executable `epdfinfo' available"))
   (add-to-list 'auto-mode-alist pdf-tools-auto-mode-alist-entry)
   (add-hook 'pdf-view-mode-hook 'pdf-tools-enable-minor-modes)
