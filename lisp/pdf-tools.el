aa233650c35b714b3365267a4274a6a27ed6ae27
Andreas Politz <politza@hochschule-trier.de>
2015-01-15 01:52:58 +0100

    Updated commentary section.
    * lisp/pdf-tools.el:

--- lisp/pdf-tools.el
+++ lisp/pdf-tools.el
@@ -23,7 +23,50 @@
 
 ;;; Commentary:
 ;;
-;; See README.
+;; PDF Tools is, among other things, a replacement of DocView for PDF
+;; files.  The key difference is, that pages are not prerendered by
+;; e.g. ghostscript and stored in the file-system, but rather created
+;; on-demand and stored in memory.
+;;
+;; Note: This package requires external libraries and works currently
+;; only on GNU/Linux systems.
+;; 
+;; Features:
+;; 
+;; * View
+;;   View PDF documents in a buffer with DocView-like bindings.
+;;   
+;; * Isearch 
+;;   Interactively search PDF documents like any other buffer. (Though
+;;   there is currently no regexp support.)
+;;   
+;; * Follow links
+;;   Click on highlighted links, moving to some part of a different
+;;   page, some external file, a website or any other URI.  Links may
+;;   also be followed by keyboard commands.
+;;   
+;; * Annotations
+;;   Display and list text and markup annotations (like underline),
+;;   edit their contents and attributes (e.g. color), move them around,
+;;   delete them or create new ones and then save the modifications
+;;   back to the PDF file.
+;;   
+;; * Attachments
+;;   Save files attached to the PDF-file or list them in a dired buffer.
+;;   
+;; * Outline
+;;   Use imenu or a special buffer to examine and navigate the PDF's
+;;   outline.
+;;   
+;; * SyncTeX
+;;   Jump from a position on a page directly to the TeX source and
+;;   vice-versa.
+;;   
+;; * Misc
+;;    + Display PDF's metadata.
+;;    + Mark a region and kill the text from the PDF.
+;;    + Search for occurrences of a string.
+;;    + Keep track of visited pages via a history.
 
 ;;; Code:
 
