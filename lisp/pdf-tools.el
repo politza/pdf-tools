d780b82a5ea5eb64140a3a68fa278f877771a1fc
Andreas Politz <politza@hochschule-trier.de>
2019-04-13 22:18:58 +0200

    Bump tablist version requirement

--- lisp/pdf-tools.el
+++ lisp/pdf-tools.el
@@ -6,7 +6,7 @@
 ;; Keywords: files, multimedia
 ;; Package: pdf-tools
 ;; Version: 1.0
-;; Package-Requires: ((emacs "24.3") (tablist "0.70") (let-alist "1.0.4"))
+;; Package-Requires: ((emacs "24.3") (tablist "1.0") (let-alist "1.0.4"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
